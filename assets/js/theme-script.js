75de5314c54c076ae9eda31bbd1939e1870c75c5
Ulan Abdurazak <ylankgz@gmail.com>
2020-03-23 19:33:06 +0600

    Add calculator

--- assets/js/theme-script.js
+++ assets/js/theme-script.js
@@ -580,3 +580,38 @@ $(window).on('load', function() {
     wowanimation();
 });
 
+
+$(document).ready(function(){
+    $('.nav-tabs > li a[title]').tooltip();
+
+    //Wizard
+    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
+
+        var $target = $(e.target);
+
+        if ($target.hasClass('disabled')) {
+            return false;
+        }
+    });
+
+    $(".next-step").click(function (e) {
+        var $active = $('.nav-tabs .nav-item .active');
+        var $activeli = $active.parent("li");
+
+        $($activeli).next().find('a[data-toggle="tab"]').removeClass("disabled");
+        $($activeli).next().find('a[data-toggle="tab"]').click();
+    });
+
+
+    $(".prev-step").click(function (e) {
+
+        var $active = $('.nav-tabs .nav-item .active');
+        var $activeli = $active.parent("li");
+
+        $($activeli).prev().find('a[data-toggle="tab"]').removeClass("disabled");
+        $($activeli).prev().find('a[data-toggle="tab"]').click();
+
+    });
+
+
+})
